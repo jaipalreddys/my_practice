266b6669311dbfd53894e7df98f08b8b19e9d010
Jaipal Reddy <jaipalreddy5000@gmail.com>
2025-03-11 20:57:28 +0530

    loops one

--- practice_07_Mar_2025.py
+++ practice_07_Mar_2025.py
@@ -1,3 +1,17 @@
-a ,b = 5,10
+# for loop practice
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
 
-print(a+b)
+sport_games = ["football", "hockey", "baseball", "cricket"]
+
+for game in board_games:
+  print(game)
+
+for sport in sport_games:
+  print(sport)
+
+# for loop with range
+
+promise = "I will finish the python loops module!"
+
+for i in range(5):
+  print(promise)
