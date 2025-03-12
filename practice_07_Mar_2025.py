c0ed1a040869bb9f3abd9dc7eab26555f08f19c9
Jaipal Reddy <jaipalreddy5000@gmail.com>
2025-03-12 21:29:03 +0530

    while loop

--- practice_07_Mar_2025.py
+++ practice_07_Mar_2025.py
@@ -15,3 +15,19 @@ promise = "I will finish the python loops module!"
 
 for i in range(5):
   print(promise)
+
+# While Loop Walkthrough
+count = 0
+print("Starting While Loop")
+while count <= 3:
+  # Loop Body
+  # Print if the condition is still true
+  print("Loop Iteration - count <= 3 is still true")
+  # Print the current value of count 
+  print("Count is currently " + str(count))
+  # Increment count
+  count += 1
+  print(" ----- ")
+print("While Loop ended")
+
+# Your code below: 
